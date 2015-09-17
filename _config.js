7a6337d05213d2c8672f8d341ec5252fa7642794
elvece <lvcifferello@gmail.com>
2015-09-17 16:01:11 -0600

    added env var

---
+++ _config.js
@@ -0,0 +1,7 @@
+var config = {};
+
+
+config.GITHUB_CLIENT_ID = 'cdbd12be63cc8b0a93f0';
+config.GITHUB_CLIENT_SECRET = 'a1f4fd9c9d289a7a091193aa62bd9b5b4e0b1f07';
+
+module.exports = config;
--- server/auth/github.js
+++ server/auth/github.js
@@ -2,13 +2,15 @@
 
 //register app on github, create a model for mongoose, setup config file/env variables
 
+var config = require('../../_config');
+
 var passport = require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
 var User = require('../models/user');
 var mongoose = require('mongoose');
 
-var GITHUB_CLIENT_ID = 'cdbd12be63cc8b0a93f0';
-var GITHUB_CLIENT_SECRET = '88e6f6dead0e7b4000ce065bf54760ea4f853451';
+var GITHUB_CLIENT_ID = config.GITHUB_CLIENT_ID;
+var GITHUB_CLIENT_SECRET = config.GITHUB_CLIENT_SECRET;
 
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
